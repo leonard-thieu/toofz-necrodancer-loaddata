74dc1facfa31e27dbb277d0c7378f346cd31c72f
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-02 17:51:56 -0400

    Migrate files from toofz-necrodancer.

---
+++ toofz.NecroDancer.LoadData/Program.cs
@@ -0,0 +1,22 @@
+﻿using toofz.NecroDancer.Data;
+using toofz.NecroDancer.EntityFramework;
+
+namespace toofz.NecroDancer.LoadData
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: This shouldn't be hardcoded.
+            var uri = @"S:\Applications\Steam\steamapps\common\Crypt of the NecroDancer\data\necrodancer.xml";
+            var data = NecroDancerDataSerializer.Read(uri);
+
+            using (var db = new NecroDancerContext())
+            {
+                db.Set<Item>().AddRange(data.Items);
+                db.Set<Enemy>().AddRange(data.Enemies);
+                db.SaveChanges();
+            }
+        }
+    }
+}
